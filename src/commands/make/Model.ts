5f9c11728b9546e1903cafd5cf285cf959fccd86
Mohamed <mohameedbenhida@gmail.com>
2018-06-25 10:50:34 +0100

    add factory and all to make:model command

--- src/commands/make/Model.ts
+++ src/commands/make/Model.ts
@@ -15,20 +15,31 @@ export default class MakeModel extends Common {
         let isController = false;
         let isMigration = false;
         let isResource = false;
+        let isFactory = false;
+        let isAll = false
 
-        // Determine if this is a resource controller or not
-        isMigration = await this.getYesNo('Should I create a migration for the model?');
+        //Determine if should we create migration,factory and resource controller
+        isAll = await this.getYesNo('Should I create migration,factory and resource controller for the model?');
+        
+        if(!isAll)
+        {
+            // Determine if we should create a migration or not
+            isMigration = await this.getYesNo('Should I create a migration for the model?');
 
-        // Should a controller be generated?
-        isController = await this.getYesNo('Should I create a controller for the model?');
-
-        // Ask if the controller is a resource if the previous answer was 'yes'
-        if (isController) {
-            // Determine if this is a resource controller or not
-            isResource = await this.getYesNo('Should I create the controller as a resource?');
+            // Determine if it should create a foctory for this model or not            
+            isFactory = await this.getYesNo('Should I create a factory for the model?');
+            
+            // Should a controller be generated?
+            isController = await this.getYesNo('Should I create a controller for the model?');
+    
+            // Ask if the controller is a resource if the previous answer was 'yes'
+            if (isController) {
+                // Determine if this is a resource controller or not
+                isResource = await this.getYesNo('Should I create the controller as a resource?');
+            }
         }
 
-        let command = `make:model ${modelName} ${isMigration ? '-m' : ''} ${isController ? '-c' : ''} ${isResource ? '-r' : ''}`;
+        let command = `make:model ${modelName} ${isMigration ? '-m' : ''} ${isFactory ? '-f' : ''} ${isController ? '-c' : ''} ${isResource ? '-r' : ''} ${isAll ? '-a' : ''}`;
 
         // Generate the model
         this.execCmd(command, async (err, stdout) => {
